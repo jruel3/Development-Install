95ce98d8c0deef1450367f960bb5c3179a5bdb34
jruel3 <72933510+jruel3@users.noreply.github.com>
2022-02-10 21:57:37 -0900

    Revision 2: Adding labels

--- install.sh
+++ install.sh
@@ -6,16 +6,43 @@ echo "Installing Homebrew"
 
 echo "Installing Github CLI"
 brew install gh
+
+echo "Installing JDK"
 brew install openjdk
+
+echo "Installing Zoom"
 brew install --cask zoom
+
+echo "Installing Node.js"
 brew install node
+
+echo "Installing pyenv"
 brew install pyenv
+
+echo "Installing Python 3.9.10"
+pyenv install 3.9.10
+
+echo "Installing Blender"
 brew install --cask blender
+
+echo "Installing Unity"
 brew install --cask unity
+
+echo "Installing Android Studio"
 brew install --cask android-studio
+
+echo "Installing Intellij CE"
 brew install --cask intellij-idea-ce
+
+echo "Installing VNC Viewer"
 brew install --cask vnc-viewer
+
+echo "Installing Libre Office Suite"
 brew install --cask libreoffice
-brew install --cask visual-studio-cOde
+
+echo "Installing VS Code"
+brew install --cask visual-studio-code
+
+echo "Installing Arduino IDE"
 brew install --cask arduino
 
